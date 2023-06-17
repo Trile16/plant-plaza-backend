6eaa2b00ba238da822c3e743b5bcc9a415ebe0c1
Tri <16trile@gmail.com>
2023-06-17 15:34:37 -0500

    changed url in db/index

--- db/index.js
+++ db/index.js
@@ -1,4 +1,14 @@
 const { Client } = require("pg");
-const client = new Client("postgres://localhost:5432/plant-plaza");
+
+const DB_NAME = "plant-plaza";
+const DB_URL =
+  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+const client = new Client({
+  connectionString: DB_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 module.exports = client;
